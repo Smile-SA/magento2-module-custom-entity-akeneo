0235a78bd3f3f4eaad413b8b7b2f4c958fe54bf8
Dmytro KHRUSHCH <dmytro.khrushch@smile-ukraine.com>
2024-04-01 14:34:27 +0300

    Fix import of smile_custom_entity type attributes  when frontend input type does not match between akeneo and magento

--- Plugin/Connector/Job/AttributePlugin.php
+++ Plugin/Connector/Job/AttributePlugin.php
@@ -20,7 +20,7 @@ class AttributePlugin
     public function __construct(
         protected Entities $entitiesHelper,
         protected EavSetup $eavSetup,
-        protected Config $configHelper
+        protected Config   $configHelper
     ) {
     }
 
@@ -33,33 +33,49 @@ class AttributePlugin
     {
         if (!$this->configHelper->isReferenceEntitiesEnabled()) {
             $connection = $this->entitiesHelper->getConnection();
-            $tmpTable = $this->entitiesHelper->getTableName('attribute');
             $entityTable = $this->entitiesHelper->getTable('akeneo_connector_entities');
-            $select = $connection->select()->from($tmpTable)
-                ->where("frontend_input = 'smile_custom_entity'");
+            $select = $connection->select()
+                ->from(
+                    ['tmp' => $this->entitiesHelper->getTableName('attribute')],
+                    [
+                        '_entity_id',
+                        'reference_data_name',
+                        'frontend_input',
+                    ]
+                )->joinLeft(
+                    ['eav' => $this->entitiesHelper->getTable('eav_attribute')],
+                    'tmp._entity_id = eav.attribute_id',
+                    [
+                        'magento_frontend_input' => 'frontend_input',
+                    ]
+                )->where("tmp.frontend_input = 'smile_custom_entity'");
             $attributes = $connection->fetchAll($select);
             $productEntityTypeId = $this->eavSetup->getEntityTypeId(ProductAttributeInterface::ENTITY_TYPE_CODE);
             foreach ($attributes as $attribute) {
-                if ($attribute['reference_data_name']) {
-                    $entitySelect = $connection->select()->from($entityTable, ['entity_id'])
-                        ->where('code = ?', $attribute['reference_data_name'])
-                        ->where('import = "smile_custom_entity"');
-                    $customEntityTypeId = $connection->fetchOne($entitySelect);
-                    if (!$customEntityTypeId) {
-                        $customEntityTypeId = $this->eavSetup->getDefaultAttributeSetId(
-                            CustomEntityInterface::ENTITY
-                        );
-                    }
-                    $values = [
-                        'is_html_allowed_on_front' => 1,
-                        'custom_entity_attribute_set_id' => $customEntityTypeId,
-                    ];
-                    $this->eavSetup->updateAttribute(
-                        $productEntityTypeId,
-                        $attribute['_entity_id'],
-                        $values
+                if (
+                    !$attribute['reference_data_name'] || !$attribute['magento_frontend_input']
+                    || $attribute['frontend_input'] !== $attribute['magento_frontend_input']
+                ) {
+                    continue;
+                }
+                $entitySelect = $connection->select()->from($entityTable, ['entity_id'])
+                    ->where('code = ?', $attribute['reference_data_name'])
+                    ->where('import = "smile_custom_entity"');
+                $customEntityTypeId = $connection->fetchOne($entitySelect);
+                if (!$customEntityTypeId) {
+                    $customEntityTypeId = $this->eavSetup->getDefaultAttributeSetId(
+                        CustomEntityInterface::ENTITY
                     );
                 }
+                $values = [
+                    'is_html_allowed_on_front' => 1,
+                    'custom_entity_attribute_set_id' => $customEntityTypeId,
+                ];
+                $this->eavSetup->updateAttribute(
+                    $productEntityTypeId,
+                    $attribute['_entity_id'],
+                    $values
+                );
             }
         }
     }
